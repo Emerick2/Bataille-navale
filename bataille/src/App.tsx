140664c78099b4ada8e16d10b45c7d07a5d5e13b
Emerick <pacaudemerick@gmail.com>
2026-09-15 16:21:20 +0200

    Mise à jours et ajout de lien avec l'API

--- bataille/src/App.tsx
+++ bataille/src/App.tsx
@@ -13,32 +13,44 @@ import NotFound from './pages/NotFound/NotFound'
 const TestAPI = async () => {
   try {
     // se créé un compte :
-    const reponse = await fetch("http://localhost:8000/auth/signup", {
-      method: "POST",
-      body: JSON.stringify({ "email": "a@b.com", "password": "hunter2", "profilePicture": "data:image/png;base64,..." }),
-    });
-    console.log(reponse);
+    // console.log("Création du compte :");
+    // const reponse = await fetch("http://localhost:8000/auth/signup", {
+    //   method: "POST",
+    //   body: JSON.stringify({ "email": "a@b.com", "password": "hunter2", "profilePicture": "data:image/png;base64,..." }),
+    // });
+    // console.log(reponse);
 
+
+    /*
     // se connecter au compte :
+    console.log("Connexion :");
     const reponse2 = await fetch("http://localhost:8000/auth/login", {
       method: "POST",
       body: JSON.stringify({ "email": "a@b.com", "password": "hunter2" }),
     });
     console.log(reponse2);
-
+    
     // // se créé une partie :
-    // const reponse3 = await fetch("http://localhost:8000/games", {
-    //   method: "POST",
-    //   body: JSON.stringify({ "minPlayers": 0, "maxPlayers": 2 }),
-    // });
-    // console.log(reponse3);
-
+    console.log("Création de la partie :");
+    const reponse3 = await fetch("http://localhost:8000/games", {
+      method: "POST",
+      body: JSON.stringify({ "minPlayers": 1, "maxPlayers": 1 }),
+    });
+    console.log(reponse3);
+    
     // // lancer la partie :
-    // const reponse4 = await fetch("http://localhost:8000/games", {
-    //   method: "POST",
-    //   body: JSON.stringify({ "state": "{...état initial...}", "currentTurnUserId": 1 }),
-    // });
-    // console.log(reponse4);
+    console.log("Lancement de la partie :");
+    const reponse4 = await fetch("http://localhost:8000/games", {
+      method: "POST",
+      body: JSON.stringify({ "state": "{...état initial...}", "currentTurnUserId": 0 }),
+    });
+    console.log(reponse4);
+    */
+    
+
+
+
+
 
 
     // const reponse5 = await fetch("http://localhost:8000/games/mine");
