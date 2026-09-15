123f0b6380abf8f29531303513a0372771abf7cf
Pauloo10__ <bihawauhpaulelie.kouakou@ynov.com>
2026-09-15 16:23:43 +0200

    ajout des pages d'inscription et de connexion

--- bataille/src/pages/Login/Login.tsx
+++ bataille/src/pages/Login/Login.tsx
@@ -1,7 +1,28 @@
+import styles from './login.module.css'
 
-const Login = () => {
-    return <h1>(page Connexion ) Le code de connexion doit être écrit par celui qui est concerné sur cette page.</h1>
+export default function Login() {
+    return (
+        <div>
+            <div className={styles.container}>
+                <h2>Veuillez vous connectez !</h2> 
+                <form action="" className={styles.form} method="post"> 
+                    <div>
+                        <label htmlFor="">Email</label>
+                        <input type="email" className={styles.input}></input>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Mot De Passe</label>
+                        <input type="password" className={styles.input}></input>
+                    </div>
+                </form>
+
+                <div>
+                    <button className={styles.boutton}>
+                        Se connecter
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
-
-
-export default Login
--- bataille/src/pages/Register/Register.tsx
+++ bataille/src/pages/Register/Register.tsx
@@ -1,6 +1,38 @@
-const Register = () => {
-    return <h1>(page Inscription ) Sur cette page, la personne concernée est autorisée à écrire le code d'inscription.</h1>
-}
+import styles from './register.module.css'
 
+export default function Register() {
+    return (
+        <div>
+            <div className={styles.container}>
+                <h2>Veuillez vous inscire</h2>
+                <form action="" className={styles.form}  method="post">
+                    <div>
+                        <label htmlFor="">Nom</label>
+                        <input type="text" className={styles.input}></input>
+                    </div>
 
-export default Register
+                    <div>
+                        <label htmlFor="">Prénom</label>
+                        <input type="text" className={styles.input}></input>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Email</label>
+                        <input type="email" className={styles.input}></input>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Mot De Passe</label>
+                        <input type="password" className={styles.input}></input>
+                    </div>
+
+                    <div>
+                        <button className={styles.boutton}>
+                            S'inscrire
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+} 
