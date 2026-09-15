cfbfc15d26e70e4536b2eac5cb00665fc36bd2bc
Emerick <pacaudemerick@gmail.com>
2026-09-15 13:10:07 +0200

    Ajout de la position de nos bateau

--- bataille/src/GameGrid.tsx
+++ bataille/src/GameGrid.tsx
@@ -3,6 +3,19 @@ import React from 'react';
 
 const myGameGrid = [
     [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0],
+    [0,1,1,0,0,0,0,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,1,1]
+]
+
+const gameGridPlay = [
+    [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -36,20 +49,45 @@ const mettreOuiSurFalse = (line : number[]) => {
     return valeur;
 }
 
-
-function GameGrid() {
+function MyGameGrid() {
     return (
         myGameGrid.map((column, colIndex) => (
             <React.Fragment key={colIndex}>
                 <div className="lineCase">
                     {column.map((line, lineIndex) => (
-                        <article key={lineIndex} className="caseGameGride"></article>
+                        line == 0 ? <article key={lineIndex} className="caseGameGride"></article> : <article key={lineIndex} className="caseGameGride boat"></article>
                     ))}
                 </div>
-                {/* <br/> */}
             </React.Fragment>
         ))
     );
 }
+
+function GameGridPlay() {
+    return (
+        gameGridPlay.map((column, colIndex) => (
+            <React.Fragment key={colIndex}>
+                <div className="lineCase">
+                    {column.map((line, lineIndex) => (
+                        <article key={lineIndex} className="caseGameGride caseGameGrideSelected"></article>
+                    ))}
+                </div>
+            </React.Fragment>
+        ))
+    );
+}
+
+
+function GameGrid() {
+    return (
+        <section className='theGrids'>
+            <MyGameGrid/>
+            {/* <span className='espaceVide'/> */}
+            <br/><br/><br/>
+            <GameGridPlay/>
+        </section>
+    );
+}
+
 
 export default GameGrid;
